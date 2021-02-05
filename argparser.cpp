2cf8cb90aeb523e0d578c6bfd6768b2f60c30c16
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2021-02-05 13:28:03 +0000

    Update argparser.cpp (#119)

--- argparser.cpp
+++ argparser.cpp
@@ -13,7 +13,7 @@ void ArgParser::PrintUsage() const
  
 void ArgParser::PrintArgs() const
 {
-    cout << "================= Arguments: ===================" << endl;
+    cout << "================= Arguments: ==================" << endl;
     cout << setw(15) << left <<  "StartLen" << " = " << StartLen << endl;
     cout << setw(15) << left << "EndLen" << " = " << EndLen << endl;
     cout << setw(15) << left << "FirstChar1" << " = " << First1 << endl;
