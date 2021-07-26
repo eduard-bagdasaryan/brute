53b2becdeaf270f862fc3c33562f32bb30928492
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2021-07-27 00:32:30 +0300

    Update brute.cpp

--- brute.cpp
+++ brute.cpp
@@ -6,7 +6,7 @@
 void BruteHelper::CrackParallel()
 {	
     for(int i = 0; i < NumThreads; ++i)
-    {
+    { 
         shared_ptr<BrutePerformer> p(new BrutePerformer(this));
 	PVec.push_back(p);
 	std::thread thread(&BrutePerformer::Crack, p.get());
