e4160439c62ac067435514e120856bdf659d23a8
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2023-05-12 22:37:52 +0300

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
  
  
-g++ -Wall -o brute main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++0x  -pthread    
+g++ -Wall -o brute main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++0x  -pthread   
 
 
  
