2992d2709dbaf44c5a3e19f1fe8c20da5e80930d
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2024-03-06 00:35:11 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -4,7 +4,7 @@
 # Evaluating execution time for one thread
 ./brute 2 3 65-90:97-122 5c8df559fbbe5156c06996be9c34397f 1 
 
-# Evaluating execution time for four threads
+# Evaluating execution time for four threads 
 ./brute 2 3 65-90:97-122 5c8df559fbbe5156c06996be9c34397f 4  
  
  
